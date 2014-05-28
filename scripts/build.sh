31387a71192d234ef20cc0f6dfe1bfd7d1cdd8b7
alee-altiscale <alee@altiscale.com>
2014-05-28 00:15:41 -0700

    fix mock runtime cfg path

--- scripts/build.sh
+++ scripts/build.sh
@@ -135,9 +135,9 @@ mkdir -p "$WORKSPACE/var/lib/mock"
 chmod 2755 "$WORKSPACE/var/lib/mock"
 mkdir -p "$WORKSPACE/var/cache/mock"
 chmod 2755 "$WORKSPACE/var/cache/mock"
-sed "s:BASEDIR:$WORKSPACE:g" "$mock_cfg" > "$mock_cfg_runtime"
-sed -i "s:IMPALA_VERSION:$IMPALA_VERSION:g" "$mock_cfg_runtime"
-echo "ok - applying mock config $mock_cfg_runtime"
+sed "s:BASEDIR:$WORKSPACE:g" "$mock_cfg" > "$curr_dir/$mock_cfg_runtime"
+sed -i "s:IMPALA_VERSION:$IMPALA_VERSION:g" "$curr_dir/$mock_cfg_runtime"
+echo "ok - applying mock config $curr_dir/$mock_cfg_runtime"
 cat "$mock_cfg_runtime"
 mock -vvv --configdir=$curr_dir -r altiscale-impala-centos-6-x86_64.runtime --resultdir=$WORKSPACE/rpmbuild/RPMS/ --rebuild $WORKSPACE/rpmbuild/SRPMS/alti-impala-${IMPALA_VERSION}-${BUILD_TIME}.el6.src.rpm
 
