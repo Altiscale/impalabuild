b608e17f6b6ce976060b77d81b52764a72aacf15
alee-altiscale <alee@altiscale.com>
2014-05-27 17:03:11 -0700

    building v1.3.1/v1.4+ impala initial scripts

---
+++ scripts/build.sh
@@ -0,0 +1,159 @@
+#!/bin/bash
+
+curr_dir=`dirname $0`
+curr_dir=`cd $curr_dir; pwd`
+
+impala_spec="$curr_dir/impala.spec"
+
+mock_cfg="$curr_dir/altiscale-impala-centos-6-x86_64.cfg"
+mock_cfg_name=$(basename "$mock_cfg")
+mock_cfg_runtime=`echo $mock_cfg_name | sed "s/.cfg/.runtime.cfg/"`
+build_timeout=14400
+
+maven_settings="$HOME/.m2/settings.xml"
+maven_settings_spec="$curr_dir/alti-maven-settings.spec"
+
+impala_zip_file="$WORKSPACE/v1.2.2.zip"
+
+if [ -f "$curr_dir/setup_env.sh" ]; then
+  source "$curr_dir/setup_env.sh"
+fi
+
+if [ "x${BUILD_TIMEOUT}" = "x" ] ; then
+  build_timeout=14400
+else
+  build_timeout=$BUILD_TIMEOUT
+fi
+
+if [ "x${WORKSPACE}" = "x" ] ; then
+  WORKSPACE="$curr_dir/../"
+  impala_zip_file="$WORKSPACE/v1.2.2.zip"
+fi
+
+if [ ! -f "$maven_settings" ]; then
+  echo "fatal - $maven_settings DOES NOT EXIST!!!! YOU MAY PULLING IN UNTRUSTED artifact and BREACH SECURITY!!!!!!"
+  exit -9
+fi
+
+if [ ! -e "$impala_spec" ] ; then
+  echo "fail - missing $impala_spec file, can't continue, exiting"
+  exit -9
+fi
+
+cleanup_secrets()
+{
+  # Erase our track for any sensitive credentials if necessary
+  rm -f $WORKSPACE/rpmbuild/RPMS/noarch/alti-maven-settings*.rpm
+  rm -f $WORKSPACE/rpmbuild/RPMS/noarch/alti-maven-settings*.src.rpm
+  rm -f $WORKSPACE/rpmbuild/SRPMS/alti-maven-settings*.src.rpm
+  rm -rf $WORKSPACE/rpmbuild/SOURCES/alti-maven-settings*
+}
+
+env | sort
+# should switch to WORKSPACE, current folder will be in WORKSPACE/impala due to 
+# hadoop_ecosystem_component_build.rb => this script will change directory into your submodule dir
+# WORKSPACE is the default path when jenkin launches e.g. /mnt/ebs1/jenkins/workspace/impala_build_test-alee
+# If not, you will be in the $WORKSPACE/impala folder already, just go ahead and work on the submodule
+# The path in the following is all relative, if the parent jenkin config is changed, things may break here.
+pushd `pwd`
+cd $WORKSPACE/impala
+if [ "x${BRANCH_NAME}" = "x" ] ; then
+  echo "error - BRANCH_NAME is not defined. Please specify the BRANCH_NAME explicitly. Exiting!"
+  exit -9
+fi
+  echo "ok - switching to impaala branch $BRANCH_NAME and refetch the files"
+  git checkout $BRANCH_NAME
+  git fetch --all
+popd
+
+echo "ok - tar zip source file, preparing for build/compile by rpmbuild"
+
+# 
+mkdir -p $WORKSPACE/rpmbuild/{BUILD,BUILDROOT,RPMS,SPECS,SOURCES,SRPMS}/
+cp "$impala_spec" $WORKSPACE/rpmbuild/SPECS/impala.spec
+cp -rp $WORKSPACE/impala $WORKSPACE/rpmbuild/SOURCES/alti-impala
+
+pushd "$WORKSPACE/rpmbuild/SOURCES/"
+tar -cpzf alti-impala.tar.gz alti-impala
+stat alti-impala.tar.gz
+
+if [ -f "$maven_settings" ] ; then
+  mkdir -p  alti-maven-settings
+  cp "$maven_settings" alti-maven-settings/
+  tar -cvzf alti-maven-settings.tar.gz alti-maven-settings
+  cp "$maven_settings_spec" $WORKSPACE/rpmbuild/SPECS/
+fi
+
+# cp $WORKSPACE/patches/* $WORKSPACE/rpmbuild/SOURCES/
+
+popd
+
+# Build alti-maven-settings RPM separately so it doesn't get exposed to spark's SRPM or any external trace
+rpmbuild -vv -ba $WORKSPACE/rpmbuild/SPECS/alti-maven-settings.spec --define "_topdir $WORKSPACE/rpmbuild" --buildroot $WORKSPACE/rpmbuild/BUILDROOT/
+if [ $? -ne "0" ] ; then
+  echo "fail - alti-maven-settings SRPM build failed"
+  cleanup_secrets
+  exit -95
+fi
+
+
+# Explicitly define IMPALA_HOME here for build purpose
+export IMPALA_HOME=$WORKSPACE/rpmbuild/BUILD/alti-impala
+echo "ok - applying version number $IMPALA_VERSION and release number $BUILD_TIME, the pattern delimiter is / here"
+sed -i "s/IMPALA_VERSION/$IMPALA_VERSION/g" "$WORKSPACE/rpmbuild/SPECS/impala.spec"
+sed -i "s/IMPALA_USER/$IMPALA_USER/g" "$WORKSPACE/rpmbuild/SPECS/impala.spec"
+sed -i "s/IMPALA_GID/$IMPALA_GID/g" "$WORKSPACE/rpmbuild/SPECS/impala.spec"
+sed -i "s/IMPALA_UID/$IMPALA_UID/g" "$WORKSPACE/rpmbuild/SPECS/impala.spec"
+sed -i "s/BUILD_TIME/$BUILD_TIME/g" "$WORKSPACE/rpmbuild/SPECS/impala.spec"
+
+rpmbuild -vvv -bs $WORKSPACE/rpmbuild/SPECS/impala.spec \
+              --define "_topdir $WORKSPACE/rpmbuild" \
+              --buildroot $WORKSPACE/rpmbuild/BUILDROOT/
+
+if [ $? -ne "0" ] ; then
+  echo "fail - rpmbuild SRPM build failed"
+  cleanup_secrets
+  exit -96
+fi
+
+stat "$WORKSPACE/rpmbuild/SRPMS/alti-impala-${IMPALA_VERSION}-${BUILD_TIME}.el6.src.rpm"
+rpm -ivvv "$WORKSPACE/rpmbuild/SRPMS/alti-impala-${IMPALA_VERSION}-${BUILD_TIME}.el6.src.rpm"
+
+echo "ok - applying $WORKSPACE for the new BASEDIR for mock, pattern delimiter here should be :"
+# the path includeds /, so we need a diff pattern delimiter
+
+mkdir -p "$WORKSPACE/var/lib/mock"
+chmod 2755 "$WORKSPACE/var/lib/mock"
+mkdir -p "$WORKSPACE/var/cache/mock"
+chmod 2755 "$WORKSPACE/var/cache/mock"
+sed "s:BASEDIR:$WORKSPACE:g" "$mock_cfg" > "$mock_cfg_runtime"
+echo "ok - applying mock config $mock_cfg_runtime"
+cat "$mock_cfg_runtime"
+mock -vvv --configdir=$curr_dir -r altiscale-impala-centos-6-x86_64.runtime --resultdir=$WORKSPACE/rpmbuild/RPMS/ --rebuild $WORKSPACE/rpmbuild/SRPMS/alti-impala-${IMPALA_VERSION}-${BUILD_TIME}.el6.src.rpm
+
+if [ $? -ne "0" ] ; then
+  echo "fail - mock RPM build failed"
+  cleanup_secrets
+  exit -97
+fi
+
+# Delete all src.rpm in the RPMS folder since this is redundant and copied by the mock process
+rm -f $WORKSPACE/rpmbuild/RPMS/*.src.rpm  
+
+echo "ok - build Completed successfully!"
+
+cleanup_secrets
+
+exit 0
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ scripts/setup_env.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# TBD: honor system pre-defined property/variable files from 
+# /etc/hadoop/ and other /etc config for spark, hdfs, hadoop, etc
+
+if [ "x${JAVA_HOME}" = "x" ] ; then
+  export JAVA_HOME=/usr/java/default
+fi
+if [ "x${ANT_HOME}" = "x" ] ; then
+  export ANT_HOME=/opt/apache-ant
+fi
+if [ "x${MAVEN_HOME}" = "x" ] ; then
+  export MAVEN_HOME=/opt/apache-maven
+fi
+if [ "x${M2_HOME}" = "x" ] ; then
+  export M2_HOME=/opt/apache-maven
+fi
+if [ "x${M2}" = "x" ] ; then
+  export M2=${M2_HOME}/bin
+fi
+if [ "x${MAVEN_OPTS}" = "x" ] ; then
+  export MAVEN_OPTS="-Xmx2048m -XX:MaxPermSize=1024m"
+fi
+if [ "x${SCALA_HOME}" = "x" ] ; then
+  export SCALA_HOME=/opt/scala
+fi
+if [ "x${HADOOP_HOME}" = "x" ] ; then
+  export HADOOP_HOME=/opt/hadoop
+fi
+if [ "x${HADOOP_CONF_DIR}" = "x" ] ; then
+  export HADOOP_CONF_DIR=/etc/hadoop
+fi
+
+export PATH=$PATH:$M2_HOME/bin:$SCALA_HOME/bin:$ANT_HOME/bin:$JAVA_HOME/bin
+
+# Define defau;t spark uid:gid and build version
+# WARNING: the IMPALA_VERSION branch name does not align with the Git branch name branch-0.8 / trunk
+if [ "x${IMPALA_USER}" = "x" ] ; then
+  export IMPALA_USER=impala
+fi
+if [ "x${IMPALA_GID}" = "x" ] ; then
+  export IMPALA_GID=411460016
+fi
+if [ "x${IMPALA_UID}" = "x" ] ; then
+  export IMPALA_UID=411460044
+fi
+if [ "x${IMPALA_VERSION}" = "x" ] ; then
+  export IMPALA_VERSION=v1.3.1
+fi
+
+if [ "x${ALTISCALE_RELEASE}" = "x" ] ; then
+  export ALTISCALE_RELEASE=2.0.0
+else
+  export ALTISCALE_RELEASE
+fi
+
+if [ "x${BRANCH_NAME}" = "x" ] ; then
+  export BRANCH_NAME=altiscale-branch-1.3.1
+fi
+
+if [ "x${BUILD_TIMEOUT}" = "x" ] ; then
+  export BUILD_TIMEOUT=14400
+fi
+
+# The build time here is part of the release number
+# It is monotonic increasing
+BUILD_TIME=$(date +%Y%m%d%H%M)
+export BUILD_TIME
+
+# Customize build OPTS for MVN
+export MAVEN_OPTS="-Xmx2048m -XX:MaxPermSize=1024m"
+
+
+
+
